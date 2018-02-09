fe1e46db7bc72f6e0508686c1350d1deefa96a66
omar <omardass99@gmail.com>
2018-02-09 19:35:28 +0300

    add all the needed codes for the third page XML and java

---
+++ app/src/main/java/com/example/isma3el/re_codedapp/FeedFragment.java
@@ -0,0 +1,30 @@
+package com.example.isma3el.re_codedapp;
+
+/**
+ * Created by Recodedharran on 7.2.2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedFragment extends Fragment {
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+
+}
--- app/src/main/java/com/example/isma3el/re_codedapp/MainActivity.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.isma3el.re_codedapp;
 
+import android.media.Image;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private SectionsPagerAdapter mSectionspagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSectionspagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.main_container);
+        viewPager.setAdapter(mSectionspagerAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.main_activity_tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
+        setUpTabIcons ();
     }
+
+    public void setUpTabIcons () {
+        tabLayout.getTabAt(0).setIcon(R.drawable.icon);
+        tabLayout.getTabAt(1).setIcon(R.drawable.news_paper);
+        tabLayout.getTabAt(2).setIcon(R.drawable.post);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new ProfileFragment();
+                case 1:
+                    return new FeedFragment();
+                case 2:
+                    return new SharePostFragment();
+            }
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return new FeedFragment();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+    }
+
 }
+
+
+
