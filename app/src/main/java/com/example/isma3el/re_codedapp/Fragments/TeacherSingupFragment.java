3831031d9e415c4eeecd918fd3dd922278834302
ismailJusuf <ismailyoussef44@gmail.com>
2018-02-09 22:15:34 +0300

    made some required changes to sign up fragments

---
+++ app/src/main/java/com/example/isma3el/re_codedapp/Fragments/TeacherSingupFragment.java
@@ -0,0 +1,37 @@
+package com.example.isma3el.re_codedapp.Fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.isma3el.re_codedapp.R;
+
+import butterknife.ButterKnife;
+
+public class TeacherSingupFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    public TeacherSingupFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate( R.layout.fragment_teacher_singup, container, false);
+        ButterKnife.bind( view );
+        return view;
+    }
+}
