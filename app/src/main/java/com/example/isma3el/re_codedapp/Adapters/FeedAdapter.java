f44a08815f6f0a0f563a9826c00ae83d01556a97
ismailJusuf <ismailyoussef44@gmail.com>
2018-02-11 18:41:11 +0300

    added navigation drawer,edited then implemented the card

--- app/src/main/java/com/example/isma3el/re_codedapp/Adapters/FeedAdapter.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/Adapters/FeedAdapter.java
@@ -1,4 +1,4 @@
-package com.example.isma3el.re_codedapp;
+package com.example.isma3el.re_codedapp.Adapters;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
@@ -8,15 +8,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.isma3el.re_codedapp.Models.FeedCard;
+import com.example.isma3el.re_codedapp.R;
+
 import java.util.ArrayList;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Recodedharran on 9.2.2018.
  */
 
-public class FeedAdapter extends ArrayAdapter<feedCard> {
-    public FeedAdapter(Activity context, ArrayList<feedCard> resturants) {
-        super(context, 0, resturants);
+public class FeedAdapter extends ArrayAdapter<FeedCard> {
+    public FeedAdapter(Activity context, ArrayList<FeedCard> cards) {
+        super(context, 0, cards);
     }
 
 
@@ -25,53 +30,53 @@ public class FeedAdapter extends ArrayAdapter<feedCard> {
         View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.card_view_news, parent, false);
+                    R.layout.feed_card_item, parent, false);
         }
 
         // Get the {@link Tour} object located at this position in the list
-        feedCard currentTour = getItem(position);
+        FeedCard currentTour = getItem( position);
 
-        ImageView imagePost = listItemView.findViewById(R.id.add_image_post);
-        imagePost.setImageResource(currentTour.getImagePostId());
+        ImageView imagePost = listItemView.findViewById(R.id.card_image_view );
+        imagePost.setImageResource(currentTour.getImage());
 
         // ImageView personalIcon = listItemView.findViewById(R.id.personal_name_icon);
         //   personalIcon.setImageResource(currentTour.getImagPersonalIcontId());
 
-        TextView personalNameText = listItemView.findViewById(R.id.personal_name_text);
-        personalNameText.setText(currentTour.getPersonalName());
+        TextView personalNameText = listItemView.findViewById(R.id.card_user_name_text_view );
+        personalNameText.setText(currentTour.getUserName());
 
-        TextView addPostText = listItemView.findViewById(R.id.add_post_text);
-        addPostText.setText(currentTour.getAddPostText());
+        TextView addPostText = listItemView.findViewById(R.id.card_text_view );
+        addPostText.setText(currentTour.getText());
 
         // ImageView heartIcon = listItemView.findViewById(R.id.emoji_icon_heart);
         // heartIcon.setImageResource(currentTour.getImagHeartIconId());
-        TextView heartText = listItemView.findViewById(R.id.heart_text);
-        heartText.setText(currentTour.getNumberHeartText());
+        TextView heartText = listItemView.findViewById(R.id.heart_counter_text_view );
+        heartText.setText(currentTour.getHeartCounter());
 
         // ImageView happyIcon = listItemView.findViewById(R.id.emoji_icon_happy);
         // happyIcon.setImageResource(currentTour.getImagHappyIconId());
-        TextView happyText = listItemView.findViewById(R.id.happy_text);
-        happyText.setText(currentTour.getNumberHappyText());
+        TextView happyText = listItemView.findViewById(R.id.happy_counter_text_view );
+        happyText.setText(currentTour.getHappyCounter());
 
         //  ImageView winkIcon = listItemView.findViewById(R.id.emoji_icon_wink);
         //  winkIcon.setImageResource(currentTour.getImagWinkIconId());
-        TextView winkText = listItemView.findViewById(R.id.wink_text);
-        winkText.setText(currentTour.getNumberWinkText());
+        TextView winkText = listItemView.findViewById(R.id.wink_counter_text_view );
+        winkText.setText(currentTour.getWinkCounter());
 
         //  ImageView nerdIcon = listItemView.findViewById(R.id.emoji_icon_nerd);
         //  nerdIcon.setImageResource(currentTour.getImagNerdIconId());
-        TextView nerdText = listItemView.findViewById(R.id.nerd_text);
-        nerdText.setText(currentTour.getNumberNerdText());
+        TextView nerdText = listItemView.findViewById(R.id.nerd_counter_text_view );
+        nerdText.setText(currentTour.getNerdCounter());
 
         //  ImageView inloveIcon = listItemView.findViewById(R.id.emoji_icon_inlove);
         // inloveIcon.setImageResource(currentTour.getImagInloveIconId());
-        TextView inloveText = listItemView.findViewById(R.id.inlove_text);
-        inloveText.setText(currentTour.getNumberInloveText());
+        TextView inloveText = listItemView.findViewById(R.id.inlove_counter_text_view );
+        inloveText.setText(currentTour.getInLoveCounter());
 
         //  ImageView thumbsIcon = listItemView.findViewById(R.id.emoji_icon_thumbs);
         //  thumbsIcon.setImageResource(currentTour.getImagThumbsIconId());
-        TextView thumbsText = listItemView.findViewById(R.id.thumbs_text);
-        thumbsText.setText(currentTour.getNumberThumbsText());
+        TextView thumbsText = listItemView.findViewById(R.id.thumbs_up_counter_text_view );
+        thumbsText.setText(currentTour.getThumbsUpCounter());
 
         return listItemView;
     }
--- app/src/main/java/com/example/isma3el/re_codedapp/MainActivity.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.isma3el.re_codedapp;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toolbar;
 
 import com.example.isma3el.re_codedapp.Fragments.FeedFragment;
 import com.example.isma3el.re_codedapp.Fragments.ProfileFragment;
 import com.example.isma3el.re_codedapp.Fragments.SharePostFragment;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private android.support.v7.widget.Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
@@ -22,34 +30,77 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        new DrawerBuilder().withActivity( this ).build();
 
-        mSectionspagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        toolbar = findViewById( R.id.main_toolbar );
 
-        viewPager = findViewById(R.id.main_container);
-        viewPager.setAdapter(mSectionspagerAdapter);
+        mSectionspagerAdapter = new SectionsPagerAdapter( getSupportFragmentManager() );
+        viewPager = findViewById( R.id.main_container );
+        viewPager.setAdapter( mSectionspagerAdapter );
+        tabLayout = findViewById( R.id.main_activity_tabs );
+        tabLayout.setupWithViewPager( viewPager );
 
-        tabLayout = (TabLayout) findViewById(R.id.main_activity_tabs);
+        setUpTabIcons();
 
-        tabLayout.setupWithViewPager(viewPager);
-        setUpTabIcons ();
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem primaryDrawerItem = new PrimaryDrawerItem().withIdentifier( 1 )
+                .withName( "test" );
+
+        SecondaryDrawerItem item1 = new SecondaryDrawerItem().withIdentifier( 1 )
+                .withName( "login" );
+
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier( 3 )
+                .withName( "signup" );
+        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier( 4 )
+                .withName( "profile" );
+        SecondaryDrawerItem item4 = new SecondaryDrawerItem().withIdentifier( 5 )
+                .withName( "feed" );
+        SecondaryDrawerItem item5 = new SecondaryDrawerItem().withIdentifier( 6 )
+                .withName( "add new post" );
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity( this )
+                .withToolbar(toolbar)
+                .addDrawerItems(primaryDrawerItem, item1, item2, item3, item4, item5)
+                .withOnDrawerItemClickListener( new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        switch (position){
+
+                            case 1:
+                                Intent intent1 = new Intent( MainActivity.this,LoginActivity.class );
+                                startActivity( intent1 );
+                                break;
+                            case 2:
+                                Intent intent2 = new Intent( MainActivity.this,SignUpActivity.class );
+                                startActivity( intent2 );
+                                break;
+                        }
+
+                        return true;
+                    }
+                } ).build();
     }
 
-    public void setUpTabIcons () {
-        tabLayout.getTabAt(0).setIcon(R.drawable.tab_user );
-        tabLayout.getTabAt(1).setIcon(R.drawable.news_paper);
-        tabLayout.getTabAt(2).setIcon(R.drawable.post);
+    public void setUpTabIcons() {
+        tabLayout.getTabAt( 0 ).setIcon( R.drawable.tab_user );
+        tabLayout.getTabAt( 1 ).setIcon( R.drawable.news_paper );
+        tabLayout.getTabAt( 2 ).setIcon( R.drawable.post );
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(android.support.v4.app.FragmentManager fm) {
-            super(fm);
+            super( fm );
         }
 
         @Override
         public android.support.v4.app.Fragment getItem(int position) {
+
             switch (position) {
                 case 0:
                     return new ProfileFragment();
