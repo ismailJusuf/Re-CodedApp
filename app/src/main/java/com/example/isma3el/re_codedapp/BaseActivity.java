e1fa4e328e50692f11d36f262dbfd8b705a7475b
ismailJusuf <ismailyoussef44@gmail.com>
2018-02-22 10:46:17 +0300

    trying to resolve a problem

--- app/src/main/java/com/example/isma3el/re_codedapp/BaseActivity.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/BaseActivity.java
@@ -30,7 +30,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        instance=this;   
+        instance=this;
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
