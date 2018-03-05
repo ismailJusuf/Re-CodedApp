9918e2f82843f159449259d4ae8706f858ee9098
omar <omardass99@gmail.com>
2018-03-05 19:45:32 +0300

    make winner xml

---
+++ app/src/main/java/com/example/isma3el/re_codedapp/WinnerActivity.java
@@ -0,0 +1,13 @@
+package com.example.isma3el.re_codedapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner);
+    }
+}
