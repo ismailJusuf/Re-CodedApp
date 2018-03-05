72722edbc10c8d0c791b8afd7c810759be7e44a3
ismailJusuf <ismailyoussef44@gmail.com>
2018-03-05 21:20:34 +0300

    added loatinfg button

--- app/src/main/java/com/example/isma3el/re_codedapp/Fragments/SharePostFragment.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/Fragments/SharePostFragment.java
@@ -1,6 +1,5 @@
 package com.example.isma3el.re_codedapp.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -12,13 +11,8 @@ import android.widget.Toast;
 
 import com.example.isma3el.re_codedapp.Adapters.FeedAdapter;
 import com.example.isma3el.re_codedapp.BaseActivity;
-import com.example.isma3el.re_codedapp.DataRefreshListener;
-import com.example.isma3el.re_codedapp.MainActivity;
 import com.example.isma3el.re_codedapp.Models.FeedCard;
 import com.example.isma3el.re_codedapp.R;
-import com.example.isma3el.re_codedapp.SharePostActivity;
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -40,8 +34,6 @@ public class SharePostFragment extends Fragment  {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference tasksDatabaseReference;
 
-    @BindView(R.id.floating_button)
-    FloatingActionsMenu floatingActionButton;
     @BindView(R.id.teacher_list_view)
     ListView listView;
 
@@ -101,12 +93,6 @@ public class SharePostFragment extends Fragment  {
 
             }
         });
-
-
-        FloatingActionButton addedOnce = new FloatingActionButton(getContext());
-        addedOnce.setTitle("Added once");
-        floatingActionButton.addButton(addedOnce);
-
 
         return view;
     }
