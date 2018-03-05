3ac193cf90941a334d2483b0c8a287f2ece1e0d3
ismailJusuf <ismailyoussef44@gmail.com>
2018-03-06 02:17:46 +0300

    added konfetti to winner screen

--- app/src/main/java/com/example/isma3el/re_codedapp/WinnerActivity.java
+++ app/src/main/java/com/example/isma3el/re_codedapp/WinnerActivity.java
@@ -1,13 +1,36 @@
 package com.example.isma3el.re_codedapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
+
 public class WinnerActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewKonfetti)
+    KonfettiView viewKonfetti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
+        ButterKnife.bind(this);
+
+        viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(1600L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(9, 4f))
+                .setPosition(-50f, viewKonfetti.getWidth() + 1400f, -50f, -50f)
+                .stream(230, 9000L);
+
     }
 }
